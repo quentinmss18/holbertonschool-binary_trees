d4a2206028edcf24d21f7422c608713a8d4274a9
quentinmss <quentinmoussous@gmail.com>
2026-04-10 15:49:47 +0200

    Update return comment and clean up code formatting

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -5,7 +5,8 @@
  * @parent: A pointer to the node to insert the right-child in
  * @value: The value to store in the new node
  *
- * Return: A pointer to the created node, or NULL on failure or if parent is NULL
+ * Return: A pointer to the created node, or NULL on failure
+ * or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
@@ -14,21 +15,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
-	/* Utilisation de la fonction du fichier 0-binary_tree_node.c */
 	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 
-	/* Si le parent a déjà un enfant à droite */
 	if (parent->right != NULL)
 	{
-		/* L'ancien enfant devient le fils droit du nouveau nœud */
 		new_node->right = parent->right;
-		/* On met à jour le parent de l'ancien enfant */
 		new_node->right->parent = new_node;
 	}
-
-	/* Le nouveau nœud devient officiellement le fils droit du parent */
 	parent->right = new_node;
 
 	return (new_node);
